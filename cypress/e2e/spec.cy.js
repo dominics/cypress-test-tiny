aad986154b9ed39af8caaa572ababee7d639158d
Dominic Scheirlinck <dominic@varspool.com>
2023-03-10 21:17:59 +1300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,19 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.flux.ai/dominic-dev/buffered-mult?editor=pcb_2d')
+    
+    let i = 100;
+    while (i > 0) {
+      cy.log(`Waiting to crash ${i}`)
+      
+      cy.get('.threeRootElement + div button[aria-label="Grid"]', {timeout: 20_000})
+        .first()
+        .as("gridButton")
+        .should("be.visible");
+      i--;
+    }
+    
+    cy.log("Finished")
   })
 })
